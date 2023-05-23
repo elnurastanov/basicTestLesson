88de5d10371f4965742c6f9296e9df7493951d14
amirmammadov <amirmammado@gmail.com>
2023-05-23 13:15:38 +0400

    amir.comment.js was created and amir.test.js was modified

---
+++ amir.comment.js
@@ -0,0 +1,47 @@
+const store = new Map()
+const ID = 1
+
+function addComment(comment){
+    const url = new URL('/comments/add','https://dummyjson.com')
+
+    return fetch(url.toString(),{
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify(comment)
+    }).then((res)=>
+        res.json()
+    ).then((data)=>{
+        store.set(ID,data)
+        return data
+    })
+}
+
+function updateComment(id,comment){
+    const url = new URL(`/comments/${id}`,'https://dummyjson.com')
+
+    return fetch(url.toString(),{
+        method:"PATCH",
+        headers:{'Content-Type':"application/json"},
+        body:JSON.stringify(comment)
+    }).then((res)=>
+        res.json()
+    ).then((data)=>{
+        store.set(ID,data)
+        return data
+    })
+}
+
+function deleteComment(id){
+    const url = new URL(`/comments/${id}`,'https://dummyjson.com')
+
+    return fetch(url.toString(),{
+        method:'DELETE'
+    }).then((res)=>
+        res.json()
+    ).then((data)=>{
+        store.delete(ID)
+        return data
+    })
+}
+
+module.exports = {addComment,updateComment,deleteComment}
